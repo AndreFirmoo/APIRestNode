cc48910f133c99ec886dba70ae71d7f9caa5317b
Andre Firmo <andrefirmo@outlook.com>
2020-06-29 00:30:14 -0300

    Configurando requisao Post

--- Controllers/Atendimentos.js
+++ Controllers/Atendimentos.js
@@ -1,5 +1,11 @@
 module.exports = app =>{
-    app.get('/Atendimentos', (requisao, resposta) =>{
+    app.get('/Atendimentos', (requisicao, resposta) =>{
         resposta.send('Atendimentos WORKS')
+    })
+
+    app.post('/Atendimentos',(requisicao, resposta)=>{
+        console.log(requisicao.body);
+        
+        resposta.send('Post')
     })
 }
